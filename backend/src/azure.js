d5d5ad2e6a41f4255fc91fc65322dd37de18a5f1
chethack <techbuddy022@gmail.com>
2023-10-18 20:53:48 +0530

    project restructure with backend

---
+++ backend/src/azure.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+const { AZURE_ENDPOINT, AZURE_KEY } = process.env;
+
+async function analyzeText(text) {
+  const response = await axios.post(
+    `${AZURE_ENDPOINT}text/analytics/v3.0/languages`,
+    { documents: [{ id: "1", text }] },
+    {
+      headers: {
+        "Ocp-Apim-Subscription-Key": AZURE_KEY,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+}
+
+module.exports = { analyzeText };
