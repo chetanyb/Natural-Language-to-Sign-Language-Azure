16a2d47c96bff527db52295978b02c00d8146c39
chethack <techbuddy022@gmail.com>
2023-11-25 13:10:00 +0530

    added redis for caching

--- backend/src/utils/findVideo.js
+++ backend/src/utils/findVideo.js
@@ -1,9 +1,18 @@
 const videoJSON = require("../../../dataset/WLASL/WLASL_v0.3.json");
+const Redis = require("ioredis");
+const redis = new Redis();
 
 async function findVideoURL(id) {
+  // Check cache first
+  const cachedUrl = await redis.get(id);
+  if (cachedUrl) {
+    return cachedUrl;
+  }
+
   try {
-    const videoURL = await videoJSON[id].instances[1].url;
+    const videoURL = videoJSON[id].instances[1].url;
     if (videoURL) {
+      await redis.set(id, videoURL);
       return videoURL;
     } else {
       throw new Error("Video not found");
--- backend/src/utils/findWordId.js
+++ backend/src/utils/findWordId.js
@@ -1,9 +1,19 @@
 const WordIndex = require("../db/models/wordIndex");
+const Redis = require("ioredis");
+const redis = new Redis();
 
 async function findWordId(word) {
+  // Check cache first
+  const cachedId = await redis.get(word);
+  if (cachedId) {
+    return cachedId;
+  }
+
   try {
     const wordRecord = await WordIndex.findOne({ where: { word } });
     if (wordRecord) {
+      // Cache the result
+      await redis.set(word, wordRecord.id);
       return wordRecord.id;
     } else {
       throw new Error("Word not found");
